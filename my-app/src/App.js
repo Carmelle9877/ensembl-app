10114cf1ff0f12f31e97d4c0b5de1b81c3bbfe01
Carmelle Ellis <Carmelle9877>
2023-08-02 09:44:07 +0100

    files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,50 @@ import * as React from 'react';
 import logo from './logo.svg';
 import SearchBox from './SearchBox';
 import './App.css';
-
-async function logData() {
-  console.log('Fetching data...')
-  const response = await fetch("https://rest.ensembl.org/homology/symbol/human/OTX2?content-type=application/json");
-  console.log(response)
-  const genomics = await response.json();
-  console.log(genomics);
-}
+import { useState } from 'react'
 
 
 function App() {
+  const [data, setData] = useState(0);
+  const [columns, setColumns] = useState([]);
+
+  const cols = ['Protein no.', 'Species', 'Gene size'];
+
+  console.log('data', data)
+
+  function returnCols(geneticCode) {
+    console.log('Genetic code', geneticCode)
+    const colsOfInterest = ['source.protein_id', 'source.species', 'taxonomy_level', 'type'];
+    columns = []
+    for (let i in colsOfInterest) {
+      console.log(i)
+    }
+
+  }
+
+  async function logData() {
+    console.log('Fetching data...')
+    const response = await fetch("https://rest.ensembl.org/homology/symbol/human/BRCA1?content-type=application/json");
+    console.log(response)
+
+    const genomics = await response.json();
+    console.log(genomics);
+    setData(genomics)
+    console.log(data.data[0].homologies[0])
+    returnCols(data.data[0].homologies[0])
+    }
+
+    console.log('new Data', data)
+
+
   return (
     <div className="App">
       <header className="App-header">
+
         <strong className ="App-title">
           Genetic codes
         </strong>
+
         <SearchBox></SearchBox>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -35,7 +62,15 @@ function App() {
         <button
           onClick={logData}
         >
-          FETCH DATA!</button>
+          FETCH DATA!
+        </button>
+        
+        {cols.map((item) => {
+          return (
+            <p>Here is {item}</p>
+          )
+        })}
+       
       </header>
     </div>
   );
